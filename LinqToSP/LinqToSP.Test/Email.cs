05c8672fb331dc19876deaf853c11e923610cb73
Ruslan Pohomenko <rpohomenko@artezio.com>
2019-09-23 17:43:29 +0300

    sample

---
+++ LinqToSP/LinqToSP.Test/Email.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.SharePoint.Client;
+using SP.Client.Linq;
+using SP.Client.Linq.Attributes;
+using System.Collections.Generic;
+
+namespace LinqToSP.Test
+{
+    [ContentType(Name = "Email")]
+    [List(Url = "Lists/TestEmails", Title = "Test Emails")]
+    class Email : ListItemEntity
+    {
+        public Email()
+        {
+            Accounts = new SpEntitySet<Account>("Accounts");
+            Account = new SpEntityLookup<Account>("Accounts");
+            Contacts = new SpEntityLookupCollection<Contact>("Contacts");
+        }
+
+        [LookupField("Account", Required = false)]
+        //public FieldLookupValue Account { get; set; }
+
+        public SpEntityLookup<Account> Account { get; set; }
+
+        [DependentLookupField(ShowField = "WorkCity", List = "a1", LookupFieldName = "Account", Result = LookupItemResult.Value)]
+        public string AccountCity { get; set; }
+
+        //[LookupField("Contact", Required = false, IsLookupId = false, IsMultiple = true)]
+        //public FieldLookupValue[] Contact { get; set; }
+
+        [LookupField("Contact", Required = false, Result = LookupItemResult.None, IsMultiple = true)]
+        public /*FieldLookupValue[]*/ string[] Contact { get; set; }
+
+        public SpEntityLookupCollection<Contact> Contacts { get; set; }
+
+        [LookupField("Contact", Required = false, Result = LookupItemResult.Id, IsMultiple = true)]
+        public List<int> ContactId { get; set; }
+
+        [LookupField("Contact", Required = false, Result = LookupItemResult.Value, IsMultiple = true)]
+        public List<string> ContactValue { get; set; }
+
+        [LookupField("AssignedTo", IsMultiple = true)]
+        public FieldUserValue[] AssignedTo { get; set; }
+
+        public SpEntitySet<Account> Accounts { get; }
+    }
+
+    [List(Url ="Lists/Accounts")]
+    public class Account : ListItemEntity
+    {
+    }
+
+    public class Contact : ListItemEntity
+    {
+    }
+}
+
---
+++ LinqToSP/LinqToSP.Test/Model/Employee.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.SharePoint.Client;
+using SP.Client.Linq;
+using SP.Client.Linq.Attributes;
+using System.Linq;
+
+namespace LinqToSP.Test.Model
+{
+    [List(Title = "Employees", Url = "Lists/Employees")]
+    public class Employee : ListItemEntity
+    {
+        public Employee()
+        {
+            Manager = new SpEntityLookup<Employee>();
+            string query = new SpEntitySet<Employee>().Where(employee => employee.IsManager).Query();
+            Managers = new SpEntitySet<Employee>(query);
+        }
+
+        [Field(Name = "FirstName", Title = "First Name", DataType = FieldType.Text)]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Field(Name = "LastName", Title = "Last Name", DataType = FieldType.Text)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Field(Name = "Phone", DataType = FieldType.Text)]
+        public string Phone
+        {
+            get;
+            set;
+        }
+
+        [Field(Name = "Email", DataType = FieldType.Text)]
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        [Field(Name = "IsManager", Title = "Is Manager", DataType = FieldType.Boolean)]
+        public bool IsManager
+        {
+            get;
+            set;
+        }
+
+        public ISpEntityLookup<Employee> Manager
+        {
+            get;
+        }
+
+        public ISpEntitySet<Employee> Managers
+        {
+            get;
+        }
+    }
+}
